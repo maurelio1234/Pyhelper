7015c3a59d3d82b769bcf2492e99c01953697b01
Marcos Almeida <maurelio1234@gmail.com>
2014-12-13 10:19:09 +0000

    Making it work in GAE again Refactoring and making things consistent Activating debugging when running in GAE

---
+++ gae/database.py
@@ -0,0 +1,12 @@
+from google.appengine.ext import ndb
+
+class StoredValue(ndb.Model):
+  value = ndb.StringProperty()
+
+def put(id, value):
+	StoredValue(id=id, value=value).put()
+
+def get(id):
+	res = StoredValue.get_by_id(id)
+	if res: return res.value
+	else: return None
